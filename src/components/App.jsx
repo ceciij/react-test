33bc846ff250ea20591f8ca2c12da118e5d1ff75
Leopoldo Caballero <git@pitakill.net>
2020-08-19 19:49:03 -0500

    First layout

---
+++ src/components/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react';                                                                                                                                        
+
+function App(props) {
+  return (
+    <div className="App">
+      <div>
+        Mexico City, Mexico Weather
+      </div>
+      <div>
+        Mexico City, Mexico Weather
+      </div>
+      <div>
+        Mexico City, Mexico Weather
+      </div>
+      <div>
+        Mexico City, Mexico Weather
+      </div>
+      <div>
+        Mexico City, Mexico Weather
+      </div>
+      as of 19:07 Central Daylight Time
+      17° Rain 20°/13°
+      feels like 12°
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Card.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+function Card() {
+    return (
+        <div className="App">
+          <div>
+            Mexico City, Mexico Weather
+          </div>
+          <div>
+            Mexico City, Mexico Weather
+          </div>
+          <div>
+            Mexico City, Mexico Weather
+          </div>
+          <div>
+            Mexico City, Mexico Weather
+          </div>
+          <div>
+            Mexico City, Mexico Weather
+          </div>
+          as of 19:07 Central Daylight Time
+          17° Rain 20°/13°
+          feels like 12°
+        </div>
+    );
+}
+
